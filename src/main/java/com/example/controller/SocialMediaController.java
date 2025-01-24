6d6318856a3878206fec5872c595d46d35c32b4e
dat vo <datcvo1@gmail.com>
2025-01-24 18:23:11 +0000

    w

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +16,22 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-public class SocialMediaController {
 
+@RestController
+public class SocialMediaController {
+    @Autowired
+    AccountService accService;
+    MessageService msgService;
+
+    @PostMapping("/register")
+    public ResponseEntity example(@RequestBody Account newAcc){
+
+        if(accService.registration(newAcc) == null)
+            return ResponseEntity.status(409).body("Username already exists.");
+        
+        if(newAcc.getUsername().isEmpty())
+            return ResponseEntity.status(400).body("Username cannot be blank.");
+ 
+        return ResponseEntity.status(400).body("Could not create username");
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,18 @@
 package com.example.service;
 
-public class AccountService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
+public class AccountService {  
+    @Autowired
+    AccountRepository accRepo;
+
+
+    public Account registration(Account Acc) {
+        return null;
+    }
 }
